a13bf69829531a0c79d332b67b9c4cbd7c536246
jamiebb <jamieandburton@gmail.com>
2016-11-03 14:21:33 -0700

    items js, html changes

--- public/js/components/items/items.js
+++ public/js/components/items/items.js
@@ -6,11 +6,25 @@ angular.module('BackpackApp')
   controllerAs: 'itemsList'
 })
 
-function ItemsCtrl() {
+var primaryItems = [];
+var secondaryItems = [];
+
+function ItemsCtrl(BackpackService) {
+
+  var list = this;
+  list.items = BackpackService.getAllItems();
+
+  // list.items.forEach(function(item) {
+  //   if(item.itemType = 'primary') {
+  //     primaryItems.push(item);
+  //   } else {
+  //     secondaryItems.push(item);
+  //   }
+  // }) 
 
 }
 
 
-
+ItemsCtrl.$inject = ['$scope', 'BackpackService'];
 
 })()
