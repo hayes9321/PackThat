6b0cf8f2e74d529351e06509adcd31382df44b6d
hayes9321 <t.hayes8321@gmail.com>
2016-11-03 14:01:49 -0700

    added service.js

--- public/js/services.js
+++ public/js/services.js
@@ -1,4 +1,46 @@
 angular.module('BackpackApp')
-.service('BackpackService', [, function(){
+.service('BackpackService', ['$http', function($http){
 
+  this.getAllItems = function(callback) {
+    $http({
+      url: '/api/items',
+      method: 'GET'
+    }).then(function success(res) {
+      callback(res);
+    }, function error(res) {
+      console.log(res);
+    });
+  }
+
+  this.addItem = function(itemData, callback) {
+    $http.post('/api/items', itemData).then(function success(res) {
+      callback(res);
+    }, function error(res) {
+      console.log(res);
+    });
+  }
+
+  this.getItem = function(itemId, callback) {
+    $http.get('/api/items' + itemId).then(function success(res) {
+      callback(res);
+    }, function error(res) {
+      console.log(res);
+    });
+  }
+
+  this.updateItem = function(itemData, itemId, callback) {
+    $http.put('/api/items' + itemId, itemData).then(function success(res) {
+      callback(res);
+    }, function error(res) {
+      console.log(res);
+    });
+  }
+
+  this.deleteItem = function(itemId, callback) {
+    $http.delete('/api/items' + itemId).then(function success(res) {
+      callback(res);
+    }, function error(res) {
+      console.log(res);
+    });
+  }
 }]);
